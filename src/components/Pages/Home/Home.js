8d8c8f51f98d341e3d8ea36f3e9d92a969cef2b5
JazibShakoor <113052211+JazibShakoor@users.noreply.github.com>
2022-10-01 19:38:19 +0500

    Update Home.js

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,21 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import classes from "./Home.module.css";
 import SlideShow from "../../SlideShow/SlideShow";
 import HomePage from "../../Container/HomePage/HomePage";
 import useData from "../../../store/CustomHook/MainData/FetchDataHook";
 
 const Home = () => {
-  const data = useData(
+  const { datas, fetchedValue } = useData(
     "https://carsdatabase-dfaec-default-rtdb.firebaseio.com/cars.json"
   );
 
+  useEffect(() => {
+    fetchedValue();
+  }, [fetchedValue]);
+
   let displayData = (
     <React.Fragment>
-      {data && data.map((data) => <HomePage key={data.id} product={data} />)}
+      {datas && datas.map((data) => <HomePage key={data.id} product={data} />)}
     </React.Fragment>
   );
 
-  if (!data) {
+  if (!datas) {
     displayData = "Found No Data To Be Display";
   }
 
